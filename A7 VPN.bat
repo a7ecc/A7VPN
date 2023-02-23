efe9c5e5581f210de00bb71b8a5ca96466cf25dc
a7ecc <102755240+a7ecc@users.noreply.github.com>
2023-02-23 02:19:19 -0800

    Update A7 VPN.bat

--- A7 VPN.bat
+++ A7 VPN.bat
@@ -9,12 +9,7 @@ FOR /F %%A IN ('powershell.exe -Command "Get-Random -Minimum 1 -Maximum 261"') D
 rasdial "A7 VPN" /DISCONNECT >NUL
 powershell -command "Remove-VpnConnection -Force -Name 'A7 VPN'" >NUL
 powershell -command "Add-VpnConnection -Name 'A7 VPN' -ServerAddress 'public-vpn-%vpn1%.opengw.net' -TunnelType Sstp" >NUL
-cls
-echo. & echo. & echo              Connecting to A7 VPN... & echo.
+cls & echo. & echo. & echo              Connecting to A7 VPN... & echo.
 FOR /F "tokens=*" %%g IN ('rasdial "A7 VPN" "vpn" "vpn"') do (SET vpn2=%%g)
 if not "%vpn2%"=="Command completed successfully." echo  Connection failed, trying with another server... & goto server
-echo         Successfully connected to A7 VPN
-echo.
-echo               Press [Enter] to exit
-set /p press=
-exit
+echo         Successfully connected to A7 VPN & echo. & echo               Press [Enter] to exit & set /p press= & exit
