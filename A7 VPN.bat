551a08ec4dff26e62dff40c74003d376e1faab10
a7ecc <102755240+a7ecc@users.noreply.github.com>
2023-02-23 02:14:47 -0800

    Update A7 VPN.bat

--- A7 VPN.bat
+++ A7 VPN.bat
@@ -1,6 +1,8 @@
 @echo off
 mode con:cols=50 lines=20
 title A7 VPN
+ping -n 1 -l 1 google.com >nul
+if errorlevel 1 color CF & echo. & echo. & echo              No internet connection & echo. & echo               Press [Enter] to exit & set /p press= & exit
 color 1F
 :server
 FOR /F %%A IN ('powershell.exe -Command "Get-Random -Minimum 1 -Maximum 261"') DO SET vpn1=%%A
@@ -16,6 +18,6 @@ FOR /F "tokens=*" %%g IN ('rasdial "A7 VPN" "vpn" "vpn"') do (SET vpn2=%%g)
 if not "%vpn2%"=="Command completed successfully." echo  Connection failed, trying with another server... & goto server
 echo         Successfully connected to A7 VPN
 echo.
-echo            Press [Enter] to exit . . .
+echo               Press [Enter] to exit
 set /p press=
 exit
